351d6472d6d03f8860085e8395181d532adad9e4
ivancekikj <ivan.cekic10@gmail.com>
2025-09-29 22:52:12 +0200

    1-email-confirmation Remove middleware, update setting configuration, run tasks in background

--- app/email_sender.py
+++ app/email_sender.py
@@ -1,20 +1,25 @@
 import asyncio
 from email.message import EmailMessage
-from aiosmtplib import SMTP
-from app.config import SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD
+
+import aiosmtplib
+from app.config import Config
 
 
 async def send_email(to_email: str, subject: str, body: str):
     message = EmailMessage()
-    message["From"] = SMTP_USER
+    message["From"] = Config.SMTP_USER
     message["To"] = to_email
     message["Subject"] = subject
     message.set_content(body)
 
-    async with SMTP(hostname=SMTP_HOST, port=SMTP_PORT, start_tls=True) as smtp:
-        await smtp.connect()
-        await smtp.login(SMTP_USER, SMTP_PASSWORD)
-        await smtp.send_message(message)
+    await aiosmtplib.send(
+        message,
+        hostname=Config.SMTP_HOST,
+        port=Config.SMTP_PORT,
+        start_tls=True,
+        username=Config.SMTP_USER,
+        password=Config.SMTP_PASSWORD,
+    )
 
 
 async def send_bulk_emails(emails: list[str], subject: str, body: str):
